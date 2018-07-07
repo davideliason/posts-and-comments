f8c1aa9ea839cd54db8982c4b2ff07d26b136038
David Eliason <david@davethemaker.com>
2018-07-06 22:09:09 -0700

    install body-parser for accepting form input data

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const logger = require('morgan');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require('dotenv').config()
 
@@ -11,6 +16,10 @@ const User = require('./src/user');
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+
 });
 
 var port = process.env.PORT || 3000;
